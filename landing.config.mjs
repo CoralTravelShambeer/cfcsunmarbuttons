b95989091b86cc4552c84a478279ea8f15895e63
Shamber <147848650+shambeer@users.noreply.github.com>
2026-09-04 16:25:05 +0300

    first upload

---
+++ landing.config.mjs
@@ -0,0 +1,14 @@
+export default {
+  schemaVersion: 1,
+  project: {
+    name: "cfc-sunmar-buttons",
+  },
+  site: {
+    preset: "sunmar",
+  },
+  stack: {
+    script: "js",
+    markup: "html",
+    styles: "scss",
+  },
+};
